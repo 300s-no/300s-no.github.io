88a7845a89dcf915d96d6176a40376beab7379b3
kobliha55555 <uhrikmat@gmail.com>
2024-04-05 19:15:09 +0200

    Update

---
+++ pages/faq.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  const questions = [
+    {
+      question: "Want clears for your car ?",
+      answer: "Message me on the Instagram account listed above. Include info about the car, some photos & where are you from in the message to get more info if/when I could make clears for your car."
+    }
+  ]
+</script>
+
+<div class="questions">
+  {#each questions as question}
+    <div class="question">
+      {question.question}
+    </div>
+    <div class="answer"> 
+      {question.answer}
+    </div>
+  {/each}
+</div>
+
+<style>
+  @media screen and (max-width: 920px) {
+    .questions {
+      width: 60vw !important;
+    }
+  }
+
+  .questions {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    top: 60vh;
+    width: 30vw;
+    height: 40vh;
+    color: white;
+  }
+
+  .question {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: .2rem;
+    height: 1.5rem; /* Adjust as needed */
+  }
+
+  .answer {
+    text-align: center;
+  }
+</style>
--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,64 @@
 <script lang="ts">
-  
+  import FaqPage from '../pages/faq.svelte';
+  import background from '../assets/bg.webp';
 </script>
 
+<img id="background" src={background} alt="background">
 <main>
   <div id="centerContainer">
-    <a href="https://www.instagram.com//" target="_blank">
+    <a id="instagram" href="https://www.instagram.com/sebastiansmash/" target="_blank">
       <p>
-        Instragram
+        INSTAGRAM
       </p>
     </a>
   </div>
+  <FaqPage />
 </main>
 
 <style>
+  #background {
+    position: absolute;
+    z-index: -1;
+    object-fit: cover;
+    width: 100vw;
+    height: 100vh;
+    user-select: none;
+  }
+
   main {
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100vw;
     height: 100vh;
-
-    background-image: url("../assets/bg.webp");
-    background-repeat: no-repeat;
-    background-size: cover;
   }
 
   #centerContainer {
     display: flex;
+    flex-direction: column;
+    gap: .25rem;
   }
 
   a {
-    color: black;
+    color: white;
     text-decoration: none;
     font-size: 2rem;
     font-weight: bold;
     background: none;
     padding: 0.75rem;
     cursor: pointer;
-    border: 0.5px rgba(0, 0, 0, 0.502) solid;
   }
 
-  a:hover p {
+  p {
+    text-align: center;
+  }
+
+  #instagram:hover p {
     background: radial-gradient(circle at 33% 100%, #fed373 4%, #f15245 30%, #d92e7f 62%, #9b36b7 85%, #515ecf);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     text-shadow: 
-      0 0 20px rgba(254, 211, 115, 0.75),
-      2px 2px 20px rgba(241, 82, 69, 0.75),
-      -2px -2px 20px rgba(217, 46, 127, 0.75),
-      4px 4px 20px rgba(155, 54, 183, 0.75),
-      -4px -4px 20px rgba(81, 94, 207, 0.75);
+      0 0 20px rgba(217, 46, 127);
     transition: all 0.5s ease;
   }
 </style>
